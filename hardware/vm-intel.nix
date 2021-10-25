fda87bfb70ffd33c805c0bcc8a41c4d5dfdcaf5a
zimbatm <zimbatm@zimbatm.com>
2021-10-25 23:01:46 +0200

    allow to prepare the VMWare image
    This is more of a showcase of a feature which I really like with NixOS.
    
    If the CI or another machine has Nix installed, it's possible to
    generate a new VMDK directly from the NixOS configuration. Building and
    copying the VMDK would replace almost all the bootstrap steps from the
    video. A similar technique is also possible to generate AMIs and other
    system images from the same config.
    
    I wasn't able to test this unfortunately because I don't have VMWare
    installed. You might have to set a password to the user using the NixOS
    config as well in order to be able to login.

--- hardware/vm-intel.nix
+++ hardware/vm-intel.nix
@@ -4,23 +4,13 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports = [ ];
+  imports = [ "${modulesPath}/virtualisation/vmware-image.nix" ];
 
   boot.initrd.availableKernelModules = [
     "ata_piix" "mptspi" "uhci_hcd" "ehci_pci" "sd_mod" "sr_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
-
-  fileSystems."/" =
-    { device = "/dev/disk/by-label/nixos";
-      fsType = "ext4";
-    };
-
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-label/boot";
-      fsType = "vfat";
-    };
 
   swapDevices = [ ];
 
