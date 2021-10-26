3e628acf68ced6d060422e9ffe903d56d5566115
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2021-10-26 15:15:05 -0700

    Fix some hiccups on aarch64

--- machines/vm-aarch64.nix
+++ machines/vm-aarch64.nix
@@ -1,7 +1,12 @@
 { config, pkgs, ... }: {
   imports = [
+    ../modules/vmware-guest.nix
     ./vm-shared.nix
   ];
+
+  # Disable the default module and import our override. We have
+  # customizations to make this work on aarch64.
+  disabledModules = [ "virtualisation/vmware-guest.nix" ];
 
   boot.kernelPatches = [
     # https://github.com/NixOS/nixpkgs/pull/140587
@@ -26,15 +31,8 @@
     }
   ];
 
-  # Disable the default module and import our override. We have
-  # customizations to make this work on aarch64.
-  disabledModules = [ "virtualisation/vmware-guest.nix" ];
-  imports = [
-    ../../modules/vmware-guest.nix
-  ];
-
   # Interface is this on M1
-  networking.interfaces.ens192.useDHCP = true;
+  networking.interfaces.ens160.useDHCP = true;
 
   # Lots of stuff that uses aarch64 that claims doesn't work, but actually works.
   nixpkgs.config.allowUnfree = true;
