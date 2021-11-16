6f40c7921521e51501aa01807913f8c669efa4f4
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2021-11-16 12:08:50 -0800

    bat

--- users/mitchellh/home-manager.nix
+++ users/mitchellh/home-manager.nix
@@ -11,6 +11,7 @@ let sources = import ../../nix/sources.nix; in {
   # per-project flakes sourced with direnv and nix-shell, so this is
   # not a huge list.
   home.packages = [
+    pkgs.bat
     pkgs.fzf
     pkgs.git-crypt
     pkgs.htop
