71b10ebe22485b64cdc42e1e8e29e173084b4604
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2021-11-24 10:49:09 -0800

    kernel 5.15 is good to go

--- machines/vm-shared.nix
+++ machines/vm-shared.nix
@@ -1,8 +1,8 @@
 { config, pkgs, currentSystem, ... }:
 
 {
-  # We require 5.14 for VMware Fusion on M1.
-  boot.kernelPackages = pkgs.linuxPackages_5_14;
+  # We require 5.14+ for VMware Fusion on M1.
+  boot.kernelPackages = pkgs.linuxPackages_5_15;
 
   # use unstable nix so we can access flakes
   nix = {
