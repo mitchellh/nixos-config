fa8615ad68960ce19f16e0db8b5aa4aad12d9467
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2021-12-02 13:52:58 -0800

    1password

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,14 +1,17 @@
 /* This contains various packages we want to overlay. Note that the
  * other ".nix" files in this directory are automatically loaded.
  */
-self: super: {
-  consul-bin = self.callPackage ../pkgs/consul-bin.nix {};
-  create-dmg = self.callPackage ../pkgs/create-dmg.nix {};
-  nomad-bin = self.callPackage ../pkgs/nomad-bin.nix {};
-  terraform-bin = self.callPackage ../pkgs/terraform-bin.nix {};
+final: prev: {
+  consul-bin = final.callPackage ../pkgs/consul-bin.nix {};
+  create-dmg = final.callPackage ../pkgs/create-dmg.nix {};
+  nomad-bin = final.callPackage ../pkgs/nomad-bin.nix {};
+  terraform-bin = final.callPackage ../pkgs/terraform-bin.nix {};
 
   # Have to force Go 1.17 because the default is fixed to 1.16
   # for reasons in the nixpkgs repository. We'll undo this when
   # they switch.
-  go = self.go_1_17;
+  go = final.go_1_17;
+
+  # Fix 1password not working properly on Linux arm64.
+  _1password = final.callPackage ../pkgs/1password.nix {};
 }
--- users/mitchellh/home-manager.nix
+++ users/mitchellh/home-manager.nix
@@ -22,6 +22,7 @@ let sources = import ../../nix/sources.nix; in {
     pkgs.tree
     pkgs.watch
     pkgs.zathura
+    pkgs._1password
 
     pkgs.tlaplusToolbox
     pkgs.tetex
