e4d77fe7f2df6ce36b1a04d9217ed68f2a59db96
Sherif Abdalla <sherif@abdalla.uk>
2021-12-31 20:41:54 +0000

    recover hashpassword

--- users/zerodeth/nixos.nix
+++ users/zerodeth/nixos.nix
@@ -9,7 +9,7 @@
     home = "/home/zerodeth";
     extraGroups = [ "docker" "wheel" ];
     shell = pkgs.fish;
-    hashedPassword = "$6$p5nPhz3G6k$6yCK0m3Oglcj4ZkUXwbjrG403LBZkfNwlhgrQAqOospGJXJZ27dI84CbIYBNsTgsoH650C1EBsbCKesSVPSpB1";
+    hashedPassword = "6yCK0m3Oglcj4ZkUXwbjrG403LBZkfNwlhgrQAqOospGJXJZ27dI84CbIYBNsTgsoH650C1EBsbCKesSVPSpB1";
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHjOxtxbn5EivnsfDKeGaappZlcW9SGk81+i4xCZfE+y Sherif Abdalla"
     ];
