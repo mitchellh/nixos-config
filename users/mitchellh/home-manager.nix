eb1f4932b1170022145c98a2fa1f3359431cf23a
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2022-03-10 10:05:11 -0800

    update fish theme

--- users/mitchellh/home-manager.nix
+++ users/mitchellh/home-manager.nix
@@ -103,9 +103,9 @@ let sources = import ../../nix/sources.nix; in {
   programs.fish = {
     enable = true;
     interactiveShellInit = lib.strings.concatStrings (lib.strings.intersperse "\n" [
-      "source ${sources.theme-bobthefish}/fish_prompt.fish"
-      "source ${sources.theme-bobthefish}/fish_right_prompt.fish"
-      "source ${sources.theme-bobthefish}/fish_title.fish"
+      "source ${sources.theme-bobthefish}/functions/fish_prompt.fish"
+      "source ${sources.theme-bobthefish}/functions/fish_right_prompt.fish"
+      "source ${sources.theme-bobthefish}/functions/fish_title.fish"
       (builtins.readFile ./config.fish)
       "set -g SHELL ${pkgs.fish}/bin/fish"
     ]);
