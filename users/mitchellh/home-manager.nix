b8c8f5d9cbfb1ffe4193e036222adff2a80dd3b3
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2022-04-03 20:27:06 -0700

    vim-glsl

--- users/mitchellh/home-manager.nix
+++ users/mitchellh/home-manager.nix
@@ -234,6 +234,7 @@ let sources = import ../../nix/sources.nix; in {
       customVim.vim-cue
       customVim.vim-fish
       customVim.vim-fugitive
+      customVim.vim-glsl
       customVim.vim-misc
       customVim.vim-pgsql
       customVim.vim-tla
