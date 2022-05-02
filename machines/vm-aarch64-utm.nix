fd601039218b0a0d0b76b5512d79f004482d81ff
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2022-05-02 14:18:54 -0700

    utm: libgl always software

--- machines/vm-aarch64-utm.nix
+++ machines/vm-aarch64-utm.nix
@@ -9,6 +9,9 @@
   # Qemu
   services.spice-vdagentd.enable = true;
 
+  # For now, we need this since hardware acceleration does not work.
+  environment.variables.LIBGL_ALWAYS_SOFTWARE = "1";
+
   # Lots of stuff that uses aarch64 that claims doesn't work, but actually works.
   nixpkgs.config.allowUnfree = true;
   nixpkgs.config.allowUnsupportedSystem = true;
