2c5f914bf735398fd8a28ab9bd44c9027af785b5
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2022-05-03 10:09:50 -0700

    add xrandr-auto for resizing

--- machines/vm-shared.nix
+++ machines/vm-shared.nix
@@ -89,6 +89,12 @@
     niv
     rxvt_unicode
     xclip
+
+    # For hypervisors that support auto-resizing, this script forces it.
+    # I've noticed not everyone listens to the udev events so this is a hack.
+    (writeShellScriptBin "xrandr-auto" ''
+      xrandr --output Virtual-1 --auto
+    '')
   ] ++ lib.optionals (currentSystemName == "vm-aarch64") [
     # This is needed for the vmware user tools clipboard to work.
     # You can test if you don't need this by deleting this and seeing
