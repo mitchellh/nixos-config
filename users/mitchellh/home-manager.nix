b0eb878bcafcefd5ed314e25339467fe090bf65c
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2022-05-17 08:38:45 -0700

    remove nix-direnv, had a bad experience

--- users/mitchellh/home-manager.nix
+++ users/mitchellh/home-manager.nix
@@ -89,8 +89,6 @@ let sources = import ../../nix/sources.nix; in {
 
   programs.direnv= {
     enable = true;
-    nix-direnv.enable = true;
-    nix-direnv.enableFlakes = true;
 
     config = {
       whitelist = {
