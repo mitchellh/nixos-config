df3424626fa70e27181bec1c4095786ea1f51723
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2022-07-21 08:57:15 -0700

    fix "error switching console mode" message on boot

--- machines/vm-shared.nix
+++ machines/vm-shared.nix
@@ -21,6 +21,10 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  # VMware, Parallels both only support this being 0 otherwise you see
+  # "error switching console mode" on boot.
+  boot.loader.systemd-boot.consoleMode = "0";
+
   # Define your hostname.
   networking.hostName = "dev";
 
