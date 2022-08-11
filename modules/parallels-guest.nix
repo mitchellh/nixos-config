6370cae243d25db14fbc4af3a10d77399e0cea05
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2022-08-11 09:48:16 -0700

    Revert "update parallels-tools to supposedly support 5.19"
    This reverts commit fce99514fd54d33f1ec82189d641a037c031b66c.

--- modules/parallels-guest.nix
+++ modules/parallels-guest.nix
@@ -4,6 +4,7 @@ with lib;
 
 let
   prl-tools = config.hardware.parallels.package;
+  aarch64 = pkgs.stdenv.hostPlatform.system == "aarch64-linux";
 in
 
 {
@@ -45,50 +46,43 @@ in
   };
 
   config = mkIf config.hardware.parallels.enable {
-
     services.udev.packages = [ prl-tools ];
 
     environment.systemPackages = [ prl-tools ];
 
     boot.extraModulePackages = [ prl-tools ];
 
-    boot.kernelModules = [ "prl_fs" "prl_fs_freeze" "prl_tg" ]
-      ++ optional (pkgs.stdenv.hostPlatform.system == "aarch64-linux") "prl_notifier";
+    boot.kernelModules = [ "prl_fs" "prl_fs_freeze" "prl_tg" ] ++ optional aarch64 "prl_notifier";
 
-    services.timesyncd.enable = false;
+    # services.timesyncd.enable = false;
 
     systemd.services.prltoolsd = {
-      description = "Parallels Tools Service";
+      description = "Parallels Tools' service";
       wantedBy = [ "multi-user.target" ];
-      path = [ prl-tools ];
       serviceConfig = {
         ExecStart = "${prl-tools}/bin/prltoolsd -f";
         PIDFile = "/var/run/prltoolsd.pid";
-        WorkingDirectory = "${prl-tools}/bin";
       };
     };
 
     systemd.services.prlfsmountd = mkIf config.hardware.parallels.autoMountShares {
-      description = "Parallels Guest File System Sharing Tool";
+      description = "Parallels Shared Folders Daemon";
       wantedBy = [ "multi-user.target" ];
-      path = [ prl-tools ];
       serviceConfig = rec {
         ExecStart = "${prl-tools}/sbin/prlfsmountd ${PIDFile}";
         ExecStartPre = "${pkgs.coreutils}/bin/mkdir -p /media";
         ExecStopPost = "${prl-tools}/sbin/prlfsmountd -u";
         PIDFile = "/run/prlfsmountd.pid";
-        WorkingDirectory = "${prl-tools}/bin";
       };
     };
 
     systemd.services.prlshprint = {
-      description = "Parallels Printing Tool";
+      description = "Parallels Shared Printer Tool";
       wantedBy = [ "multi-user.target" ];
       bindsTo = [ "cups.service" ];
-      path = [ prl-tools ];
       serviceConfig = {
+        Type = "forking";
         ExecStart = "${prl-tools}/bin/prlshprint";
-        WorkingDirectory = "${prl-tools}/bin";
       };
     };
 
@@ -96,47 +90,37 @@ in
       prlcc = {
         description = "Parallels Control Center";
         wantedBy = [ "graphical-session.target" ];
-        path = [ prl-tools ];
         serviceConfig = {
           ExecStart = "${prl-tools}/bin/prlcc";
-          WorkingDirectory = "${prl-tools}/bin";
         };
       };
       prldnd = {
-        description = "Parallels Drag And Drop Tool";
+        description = "Parallels Control Center";
         wantedBy = [ "graphical-session.target" ];
-        path = [ prl-tools ];
         serviceConfig = {
           ExecStart = "${prl-tools}/bin/prldnd";
-          WorkingDirectory = "${prl-tools}/bin";
         };
       };
       prlcp = {
-        description = "Parallels Copy Paste Tool";
+        description = "Parallels CopyPaste Tool";
         wantedBy = [ "graphical-session.target" ];
-        path = [ prl-tools ];
         serviceConfig = {
           ExecStart = "${prl-tools}/bin/prlcp";
           Restart = "always";
-          WorkingDirectory = "${prl-tools}/bin";
         };
       };
       prlsga = {
         description = "Parallels Shared Guest Applications Tool";
         wantedBy = [ "graphical-session.target" ];
-        path = [ prl-tools ];
         serviceConfig = {
           ExecStart = "${prl-tools}/bin/prlsga";
-          WorkingDirectory = "${prl-tools}/bin";
         };
       };
       prlshprof = {
         description = "Parallels Shared Profile Tool";
         wantedBy = [ "graphical-session.target" ];
-        path = [ prl-tools ];
         serviceConfig = {
           ExecStart = "${prl-tools}/bin/prlshprof";
-          WorkingDirectory = "${prl-tools}/bin";
         };
       };
     };
