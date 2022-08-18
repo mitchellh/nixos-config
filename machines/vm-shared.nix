1b409d395fdee03f5591099e7b4279afbab75783
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2022-08-18 13:56:48 -0700

    xrandr-6k/mbp is no longer needed

--- machines/vm-shared.nix
+++ machines/vm-shared.nix
@@ -106,19 +106,6 @@
     # You can test if you don't need this by deleting this and seeing
     # if the clipboard sill works.
     gtkmm3
-
-    # VMware on M1 doesn't support automatic resizing yet and on
-    # my big monitor it doesn't detect the resolution either so we just
-    # manualy create the resolution and switch to it with this script.
-    # This script could be better but its hopefully temporary so just force it.
-    (writeShellScriptBin "xrandr-6k" ''
-      xrandr --newmode "6016x3384_60.00"  1768.50  6016 6544 7216 8416  3384 3387 3392 3503 -hsync +vsync
-      xrandr --addmode Virtual-1 6016x3384_60.00
-      xrandr -s 6016x3384_60.00
-    '')
-    (writeShellScriptBin "xrandr-mbp" ''
-      xrandr -s 2880x1800
-    '')
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
