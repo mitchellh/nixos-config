62b0e17fd6b422aa89115681f3cb43cd5711a898
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2022-08-18 10:02:45 -0700

    VMW 3D: Rebuild Mesa to include svga on aarch64
    "svga" is the VMware graphics driver (vmwgfx). The default Mesa build
    doesn't include this on aarch64[1] and Nix defers to the Mesa
    defaults[2]. We override the Mesa build to explicitly include the "svga"
    driver.
    
    This makes 3d acceleration work on VMware Fusion 22H2! The "glxinfo"
    still shows "Accelerated: no" but this appears to be a lie, since the
    graphics performance is very noticeably better. "glxgears" is 10x faster
    than llvmpipe.
    
    [1]: https://github.com/Mesa3D/mesa/blob/49efa73ba11c4cacaed0052b984e1fb884cf7600/meson.build#L192
    [2]: https://github.com/NixOS/nixpkgs/blob/71617faa487ab0d16b6cad3c20b8c4c79fb328e3/pkgs/development/libraries/mesa/default.nix#L9

--- flake.nix
+++ flake.nix
@@ -47,10 +47,25 @@
       user   = "mitchellh";
 
       overlays = overlays ++ [(final: prev: {
-        # We need the latest version of mesa for VMware Fusion
         # TODO: drop after release following NixOS 22.05
-        mesa = inputs.nixpkgs-unstable.legacyPackages.${prev.system}.mesa;
         open-vm-tools = inputs.nixpkgs-unstable.legacyPackages.${prev.system}.open-vm-tools;
+
+        # We need Mesa on aarch64 to be built with "svga". The default Mesa
+        # build does not include this: https://github.com/Mesa3D/mesa/blob/49efa73ba11c4cacaed0052b984e1fb884cf7600/meson.build#L192
+        mesa = prev.callPackage "${inputs.nixpkgs-unstable}/pkgs/development/libraries/mesa" {
+          llvmPackages = final.llvmPackages_latest;
+          inherit (final.darwin.apple_sdk.frameworks) OpenGL;
+          inherit (final.darwin.apple_sdk.libs) Xplugin;
+
+          galliumDrivers = [
+            # From meson.build
+            "v3d" "vc4" "freedreno" "etnaviv" "nouveau"
+            "tegra" "virgl" "lima" "panfrost" "swrast"
+
+            # We add this so we get the vmwgfx module
+            "svga"
+          ];
+        };
       })];
     };
 
