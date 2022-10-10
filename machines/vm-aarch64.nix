a518c6ded8df06555fb33cd5f733f58956ce222e
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2022-10-10 14:32:14 -0700

    setup binfmt_misc for x86_64 emulation

--- machines/vm-aarch64.nix
+++ machines/vm-aarch64.nix
@@ -4,6 +4,9 @@
     ./vm-shared.nix
   ];
 
+  # Setup qemu so we can run x86_64 binaries
+  boot.binfmt.emulatedSystems = ["x86_64-linux"];
+
   # Disable the default module and import our override. We have
   # customizations to make this work on aarch64.
   disabledModules = [ "virtualisation/vmware-guest.nix" ];
