0b27553509e0cc4d882bf591f78c27bb89b8dfdd
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2022-10-13 10:46:47 -0700

    update neovim

--- flake.nix
+++ flake.nix
@@ -17,8 +17,15 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    # I think technically you're not supposed to override the nixpkgs
+    # used by neovim but recently I had failures if I didn't pin to my
+    # own. We can always try to remove that anytime.
+    neovim-nightly-overlay = {
+      url = "github:nix-community/neovim-nightly-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     # Other packages
-    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
     zig.url = "github:mitchellh/zig-overlay";
   };
 
