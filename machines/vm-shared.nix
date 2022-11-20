696fc49a8f98d8418e0f41071effdd109d7c1d6c
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2022-11-19 19:31:27 -0800

    enable tailscale

--- machines/vm-shared.nix
+++ machines/vm-shared.nix
@@ -78,6 +78,11 @@
     };
   };
 
+  # Enable tailscale. We manually authenticate when we want with
+  # "sudo tailscale up". If you don't use tailscale, you should comment
+  # out or delete all of this.
+  services.tailscale.enable = true;
+
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.mutableUsers = false;
 
