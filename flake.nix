e8c4c15d6020298b7d48a89bc86330a4d3cba243
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2023-01-21 10:20:00 -0800

    update deps

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     # Pin our primary nixpkgs repository. This is the main nixpkgs repository
     # we'll use for our configurations. Be very careful changing this because
     # it'll impact your entire system.
-    nixpkgs.url = "github:nixos/nixpkgs/release-22.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
 
     # We use the unstable nixpkgs repo for some packages.
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
