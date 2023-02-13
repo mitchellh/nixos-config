efc995fa8d2fec2a1899019250370795ec70f3c7
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2023-02-12 19:58:59 -0800

    darwin: add homebrew casks

--- users/mitchellh/darwin.nix
+++ users/mitchellh/darwin.nix
@@ -5,7 +5,23 @@
     (import ./vim.nix)
   ];
 
-  homebrew.enable = true;
+  homebrew = {
+    enable = true;
+    casks  = [
+      "1password"
+      "alfred"
+      "cleanshot"
+      "discord"
+      "google-chrome"
+      "imageoptim"
+      "istat-menus"
+      "monodraw"
+      "rectangle"
+      "screenflow"
+      "slack"
+      "spotify"
+    ];
+  };
 
   # The user should already exist, but we need to set this up so Nix knows
   # what our home directory is (https://github.com/LnL7/nix-darwin/issues/423).
