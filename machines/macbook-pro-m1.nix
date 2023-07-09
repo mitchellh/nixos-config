e903e6f4c5dca35eb82426890b7142394fa37f2d
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2023-07-09 10:31:04 -0700

    remove unused comment

--- machines/macbook-pro-m1.nix
+++ machines/macbook-pro-m1.nix
@@ -1,8 +1,4 @@
 { config, pkgs, ... }: {
-  # imports = [
-  #   ./vm-shared.nix
-  # ];
-
   # We install Nix using a separate installer so we don't want nix-darwin
   # to manage it for us. This tells nix-darwin to just use whatever is running.
   nix.useDaemon = true;
