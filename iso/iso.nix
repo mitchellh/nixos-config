b4a7657f8294f1445eb9bdaef03adda44879be49
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2023-07-09 10:14:02 -0700

    remove iso dir, we can use official isos now

--- iso/iso.nix
+++
@@ -1,8 +0,0 @@
-{ config, pkgs, ... }:
-
-{
-  imports = [
-    <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix>
-    <nixpkgs/nixos/modules/installer/cd-dvd/channel.nix>
-  ];
-}
