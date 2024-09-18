34c8960b09bc25d0f612f8edc5a984717a22a204
Mitchell Hashimoto <m@mitchellh.com>
2024-09-18 07:19:15 -0700

    update deps for macOS sequoia

--- machines/macbook-pro-m1.nix
+++ machines/macbook-pro-m1.nix
@@ -1,4 +1,7 @@
 { config, pkgs, ... }: {
+  # Set in Sept 2024 as part of the macOS Sequoia release.
+  system.stateVersion = 5;
+
   # We install Nix using a separate installer so we don't want nix-darwin
   # to manage it for us. This tells nix-darwin to just use whatever is running.
   nix.useDaemon = true;
