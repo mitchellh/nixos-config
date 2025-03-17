6861264a7bd99a5d9b9d29230e1e8d5e8fe1a1a5
Mitchell Hashimoto <m@mitchellh.com>
2025-03-17 12:40:48 -0700

    enable linux builder on darwin

--- machines/macbook-pro-m1.nix
+++ machines/macbook-pro-m1.nix
@@ -15,12 +15,38 @@
       keep-derivations = true
     '';
 
+    # Enable the Linux builder so we can run Linux builds on our Mac.
+    # This can be debugged by running `sudo ssh linux-builder`
+    linux-builder = {
+      enable = true;
+      ephemeral = true;
+      maxJobs = 4;
+      config = ({ pkgs, ... }: {
+        # Make our builder beefier since we're on a beefy machine.
+        virtualisation = {
+          cores = 6;
+          darwin-builder = {
+            diskSize = 100 * 1024; # 100GB
+            memorySize = 32 * 1024; # 32GB
+          };
+        };
+
+        # Add some common debugging tools we can see whats up.
+        environment.systemPackages = [
+          pkgs.htop
+        ];
+      });
+    };
+
     # public binary cache that I use for all my derivations. You can keep
     # this, use your own, or toss it. Its typically safe to use a binary cache
     # since the data inside is checksummed.
     settings = {
       substituters = ["https://mitchellh-nixos-config.cachix.org"];
       trusted-public-keys = ["mitchellh-nixos-config.cachix.org-1:bjEbXJyLrL1HZZHBbO4QALnI5faYZppzkU4D2s0G8RQ="];
+
+      # Required for the linux builder
+      trusted-users = ["@admin"];
     };
   };
 
