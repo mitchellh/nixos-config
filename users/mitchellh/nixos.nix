9e246a28cd3f99281bafb7b2e7401ef152fa3672
Mitchell Hashimoto <m@mitchellh.com>
2025-04-03 04:26:00 -0700

    upgrade

--- users/mitchellh/nixos.nix
+++ users/mitchellh/nixos.nix
@@ -14,7 +14,7 @@
     isNormalUser = true;
     home = "/home/mitchellh";
     extraGroups = [ "docker" "lxd" "wheel" ];
-    shell = pkgs.nushell;
+    shell = pkgs.fish;
     hashedPassword = "$6$p5nPhz3G6k$6yCK0m3Oglcj4ZkUXwbjrG403LBZkfNwlhgrQAqOospGJXJZ27dI84CbIYBNsTgsoH650C1EBsbCKesSVPSpB1";
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGbTIKIPtrymhvtTvqbU07/e7gyFJqNS4S0xlfrZLOaY mitchellh"
